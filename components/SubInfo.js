6169d6d59e2dcbfee6df17dd7712571386535ba6
icecrimu <charles.banigon@gmail.com>
2022-04-03 23:44:21 +0800

    Add EndDate component

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -28,3 +28,40 @@ export const People = () => {
     </View>
   )
 }
+
+export const EndDate = () => {
+  return (
+    <View
+      style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent: "center",
+        alignItems: "center",
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: "50%"
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.primary
+        }}
+      >
+        Ending in
+      </Text>
+
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.medium,
+          color: COLORS.primary
+        }}
+      >
+        12h 30m
+      </Text>
+    </View>
+  )
+}
