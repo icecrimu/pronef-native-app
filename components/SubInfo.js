66f40ba775b4298dda32aea8ac73f81a8c21791d
icecrimu <charles.banigon@gmail.com>
2022-04-03 23:42:44 +0800

    Add ImageCmp, People component

---
+++ components/SubInfo.js
@@ -0,0 +1,30 @@
+import { View, Text, Image } from "react-native"
+import React from "react"
+
+import { COLORS, FONTS, SIZES, SHADOWS, assets } from "../constants"
+
+export const ImageCmp = ({ imgUrl, index }) => {
+  return (
+    <Image
+      source={imgUrl}
+      resizeMode="contain"
+      style={{
+        width: 48,
+        height: 48,
+        marginLeft: index === 0 ? 0 : -SIZES.font
+      }}
+    />
+  )
+}
+
+export const People = () => {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {[assets.person02, assets.person03, assets.person04].map(
+        (imgUrl, index) => (
+          <ImageCmp imgUrl={imgUrl} index={index} key={`People-${index}`} />
+        )
+      )}
+    </View>
+  )
+}
