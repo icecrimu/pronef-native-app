fc131f5d2f78eca56758a9476eb62dd368130419
icecrimu <charles.banigon@gmail.com>
2022-04-04 00:18:45 +0800

    Fix layout View

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -39,22 +39,22 @@ const NFTCard = ({ data }) => {
           titleSize={SIZES.large}
           subTitleSize={SIZES.small}
         />
-      </View>
 
-      <View
-        style={{
-          marginTop: SIZES.font,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center"
-        }}
-      >
-        <EthPrice price={data.price} />
-        <RectButton
-          minWidth={120}
-          fontSize={SIZES.font}
-          handlePress={() => navigation.navigate("Details", { data })}
-        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center"
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
       </View>
     </View>
   )
