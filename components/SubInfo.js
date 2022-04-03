01767ab952db0c328f7d81f83b3fd4050ecd0240
icecrimu <charles.banigon@gmail.com>
2022-04-03 23:44:58 +0800

    Add SubInfo component

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -65,3 +65,20 @@ export const EndDate = () => {
     </View>
   )
 }
+
+export const SubInfo = () => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        paddingHorizontal: SIZES.font,
+        marginTop: -SIZES.extraLarge,
+        flexDirection: "row",
+        justifyContent: "space-between"
+      }}
+    >
+      <People />
+      <EndDate />
+    </View>
+  )
+}
