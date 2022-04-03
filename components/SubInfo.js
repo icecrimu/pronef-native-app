b161a3da6d88fbf5f4eb008a921fb06f98ae997b
icecrimu <charles.banigon@gmail.com>
2022-04-03 23:45:37 +0800

    Add NFTTitle component

--- components/SubInfo.js
+++ components/SubInfo.js
@@ -3,6 +3,32 @@ import React from "react"
 
 import { COLORS, FONTS, SIZES, SHADOWS, assets } from "../constants"
 
+export const NFTTitle = ({ title, subTitle, titleSize, subTitleSize }) => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: titleSize,
+          color: COLORS.primary
+        }}
+      >
+        {title}
+      </Text>
+
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: subTitleSize,
+          color: COLORS.primary
+        }}
+      >
+        {subTitle}
+      </Text>
+    </View>
+  )
+}
+
 export const ImageCmp = ({ imgUrl, index }) => {
   return (
     <Image
