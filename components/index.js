a5eee51202e5908f0b727c0724ab973724c84ce6
icecrimu <charles.banigon@gmail.com>
2022-04-05 21:03:57 +0800

    Add DetailsBid, DetailsDesc structure

--- components/index.js
+++ components/index.js
@@ -2,5 +2,15 @@ import HomeHeader from "./HomeHeader"
 import FocusedStatusBar from "./FocusedStatusBar"
 import NFTCard from "./NFTCard"
 import { CircleButton, RectButton } from "./Button"
+import DetailsDesc from "./DetailsDesc"
+import DetailsBid from "./DetailsBid"
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton, RectButton }
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc
+}
