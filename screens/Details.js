28b84476b47734d785b06aa2694cc5e5434e9552
icecrimu <charles.banigon@gmail.com>
2022-04-05 22:14:06 +0800

    Add DetailsDesc component to Details screen

--- screens/Details.js
+++ screens/Details.js
@@ -73,6 +73,9 @@ const Details = ({ route, navigation }) => {
           <>
             <DetailsHeader data={data} navigation={navigation} />
             <SubInfo />
+            <View style={{ padding: SIZES.font }}>
+              <DetailsDesc data={data} />
+            </View>
           </>
         )}
       />
