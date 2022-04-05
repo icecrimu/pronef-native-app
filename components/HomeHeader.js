f61a8c2969d1cac781e34e880125129b9fadb4ee
icecrimu <charles.banigon@gmail.com>
2022-04-05 19:02:04 +0800

    Add header search input

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -64,6 +64,31 @@ const HomeHeader = ({ onSearch }) => {
           Let's find a masterpiece
         </Text>
       </View>
+
+      <View>
+        <View
+          style={{
+            width: "100%",
+            borderRadius: SIZES.font,
+            backgroundColor: COLORS.gray,
+            flexDirection: "row",
+            alignItems: "center",
+            paddingHorizontal: SIZES.font,
+            paddingVertical: SIZES.small - 2
+          }}
+        >
+          <Image
+            source={assets.search}
+            resizeMode="contain"
+            style={{ width: 20, height: 20, marginRight: SIZES.base }}
+          />
+          <TextInput
+            placeholder="Search NFTs"
+            style={{ flex: 1 }}
+            onChangeText={onSearch}
+          />
+        </View>
+      </View>
     </View>
   )
 }
