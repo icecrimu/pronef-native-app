bba7276c2acff49e3002dc11c381484beb73ce55
icecrimu <charles.banigon@gmail.com>
2022-04-05 21:43:30 +0800

    Add DetailsHeader

--- screens/Details.js
+++ screens/Details.js
@@ -17,6 +17,27 @@ import {
   DetailsBid
 } from "../components"
 
+const DetailsHeader = ({ data, navigation }) => (
+  <View style={{ width: "100%", height: 373 }}>
+    <Image
+      source={data.image}
+      resizeMode="cover"
+      style={{ width: "100%", height: "100%" }}
+    />
+    <CircleButton
+      imgUrl={assets.left}
+      handlePress={() => navigation.goBack()}
+      left={15}
+      top={StatusBar.currentHeight + 10}
+    />
+
+    <CircleButton
+      imgUrl={assets.heart}
+      right={15}
+      top={StatusBar.currentHeight + 10}
+    />
+  </View>
+)
 const Details = ({ route, navigation }) => {
   const { data } = route.params
 
@@ -25,7 +46,7 @@ const Details = ({ route, navigation }) => {
       <FocusedStatusBar
         barStyle="dark-content"
         backgroundColor="transparent"
-        transLucent={true}
+        translucent={true}
       />
       <View
         style={{
@@ -41,6 +62,19 @@ const Details = ({ route, navigation }) => {
       >
         <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
       </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <>
+            <DetailsHeader data={data} navigation={navigation} />
+          </>
+        )}
+      />
     </SafeAreaView>
   )
 }
