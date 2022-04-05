a348382895ee45bf7eef14c2bf7f1bf4c5255ab7
icecrimu <charles.banigon@gmail.com>
2022-04-05 18:59:04 +0800

    Add logo, profile, badge

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,47 @@
-import { View, Text } from "react-native"
-import React from "react"
+import { View, Text, Image, TextInput } from "react-native"
 
-const HomeHeader = () => {
+import { COLORS, FONTS, SIZES, assets } from "../constants"
+
+const HomeHeader = ({ onSearch }) => {
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.font
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignContent: "center"
+        }}
+      >
+        <Image
+          source={assets.logo}
+          resizeMode="contain"
+          style={{ width: 90, height: 25 }}
+        />
+
+        <View style={{ width: 45, height: 45 }}>
+          <Image
+            source={assets.person01}
+            resizeMode="contain"
+            style={{ width: "100%", height: "100%" }}
+          />
+          <Image
+            source={assets.badge}
+            resizeMode="contain"
+            style={{
+              position: "absolute",
+              width: 15,
+              height: 15,
+              bottom: 0,
+              right: 0
+            }}
+          />
+        </View>
+      </View>
     </View>
   )
 }
